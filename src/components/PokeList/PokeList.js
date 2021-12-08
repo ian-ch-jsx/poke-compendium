5fa18b69cf19a757ea78f7343f3fe86b99d628d9
Ian Christiansen <ianc.songdog@gmail.com>
2021-12-08 14:45:39 -0800

    styling

--- src/components/PokeList/PokeList.js
+++ src/components/PokeList/PokeList.js
@@ -2,12 +2,14 @@ import './PokeList.css';
 
 export default function PokeList({ pokemon }) {
   return (
-    <div className="poke-card">
+    <div className="container">
       {pokemon.map((poke) => (
         <>
-          <h3 key={poke.id}>{poke.pokemon}</h3>
-          <img src={poke.url_image} className="image"></img>
-          <p key={poke.id}>type: {poke.type_1}</p>
+          <div className="poke-card">
+            <h3 key={poke.id}>{poke.pokemon}</h3>
+            <img src={poke.url_image} className="image"></img>
+            <p key={poke.id}>type: {poke.type_1}</p>
+          </div>
         </>
       ))}
     </div>
