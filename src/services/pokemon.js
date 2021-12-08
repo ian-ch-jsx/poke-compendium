32831fdd868f7456f526539c2332cb1cd0bf2103
Ian Christiansen <ianc.songdog@gmail.com>
2021-12-08 15:25:49 -0800

    pokemon sorted by ID by default

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -2,7 +2,7 @@ export async function getPokemon(query) {
   const params = new URLSearchParams();
   params.set('pokemon', query);
   const resp = await fetch(
-    `https://pokedex-alchemy.herokuapp.com/api/pokedex?${params.toString()}`
+    `https://pokedex-alchemy.herokuapp.com/api/pokedex?${params.toString()}&perPage=48&sort=id&direction=asc`
   );
   const data = await resp.json();
   return data;
