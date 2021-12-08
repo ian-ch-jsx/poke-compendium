30f3546f7cebd475405ac0ecca018118f206162e
Ian Christiansen <ianc.songdog@gmail.com>
2021-12-08 14:10:10 -0800

    added styling

--- src/components/PokeList/PokeList.js
+++ src/components/PokeList/PokeList.js
@@ -1,8 +1,14 @@
+import './PokeList.css';
+
 export default function PokeList({ pokemon }) {
   return (
-    <div>
+    <div className="poke-card">
       {pokemon.map((poke) => (
-        <p key={poke.id}>{poke.pokemon}</p>
+        <>
+          <h3 key={poke.id}>{poke.pokemon}</h3>
+          <img src={poke.url_image} className="image"></img>
+          <p key={poke.id}>type: {poke.type_1}</p>
+        </>
       ))}
     </div>
   );
