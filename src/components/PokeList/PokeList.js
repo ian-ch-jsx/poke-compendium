36490be17f17f37b2d392b1fc2bcc1465ceaf861
Ian Christiansen <ianc.songdog@gmail.com>
2021-12-08 17:04:59 -0800

    added additional info to pokecards

--- src/components/PokeList/PokeList.js
+++ src/components/PokeList/PokeList.js
@@ -9,6 +9,10 @@ export default function PokeList({ pokemon }) {
             <h3 key={poke.id}>{poke.pokemon}</h3>
             <img src={poke.url_image} className="image"></img>
             <p key={poke.id}>type: {poke.type_1}</p>
+            <p key={poke.id}>generation {poke.generation_id}</p>
+            <a key={poke.id} href={poke.pokedex} target="_blank" rel="noopener noreferrer">
+              pokedex entry
+            </a>
           </div>
         </>
       ))}
