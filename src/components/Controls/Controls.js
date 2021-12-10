5bca9f0ce7e1d57bb8b9493938a50058e7e55ede
Ian Christiansen <ianc.songdog@gmail.com>
2021-12-09 16:15:44 -0800

    added mui

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -1,5 +1,5 @@
-// import { getPokemon } from '../../services/pokemon';
 import './Controls.css';
+import { Select, MenuItem, InputLabel, FormControl } from '@mui/material';
 
 export default function Controls({
   query,
@@ -20,31 +20,46 @@ export default function Controls({
           setQuery(e.target.value);
         }}
       />
-      <select value={selectedType} onChange={(e) => setSelectedType(e.target.value)}>
-        <option value="all">All</option>
-        <option value="bug">Bug</option>
-        <option value="dark">Dark</option>
-        <option value="dragon">Dragon</option>
-        <option value="electric">Electric</option>
-        <option value="fairy">Fairy</option>
-        <option value="fighting">Fighting</option>
-        <option value="fire">Fire</option>
-        <option value="flying">Flying</option>
-        <option value="ghost">Ghost</option>
-        <option value="grass">Grass</option>
-        <option value="ground">Ground</option>
-        <option value="ice">Ice</option>
-        <option value="normal">Normal</option>
-        <option value="poison">Poison</option>
-        <option value="psychic">Psychic</option>
-        <option value="rock">Rock</option>
-        <option value="steel">Steel</option>
-        <option value="water">Water</option>
-      </select>
-      <select value={order} onChange={(e) => setOrder(e.target.value)}>
-        <option value="asc">Ascending</option>
-        <option value="desc"> Descending</option>
-      </select>
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        <Select
+          className="select"
+          labelId="type-label"
+          label="type"
+          value={selectedType}
+          onChange={(e) => setSelectedType(e.target.value)}
+        >
+          <MenuItem value="all">All</MenuItem>
+          <MenuItem value="bug">Bug</MenuItem>
+          <MenuItem value="dark">Dark</MenuItem>
+          <MenuItem value="dragon">Dragon</MenuItem>
+          <MenuItem value="electric">Electric</MenuItem>
+          <MenuItem value="fairy">Fairy</MenuItem>
+          <MenuItem value="fighting">Fighting</MenuItem>
+          <MenuItem value="fire">Fire</MenuItem>
+          <MenuItem value="flying">Flying</MenuItem>
+          <MenuItem value="ghost">Ghost</MenuItem>
+          <MenuItem value="grass">Grass</MenuItem>
+          <MenuItem value="ground">Ground</MenuItem>
+          <MenuItem value="ice">Ice</MenuItem>
+          <MenuItem value="normal">Normal</MenuItem>
+          <MenuItem value="poison">Poison</MenuItem>
+          <MenuItem value="psychic">Psychic</MenuItem>
+          <MenuItem value="rock">Rock</MenuItem>
+          <MenuItem value="steel">Steel</MenuItem>
+          <MenuItem value="water">Water</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        <Select
+          className="select"
+          labelId="order-label"
+          value={order}
+          onChange={(e) => setOrder(e.target.value)}
+        >
+          <MenuItem value="asc">Ascending</MenuItem>
+          <MenuItem value="desc"> Descending</MenuItem>
+        </Select>
+      </FormControl>
       <button onClick={() => setLoading(true)}>Search</button>
     </div>
   );
